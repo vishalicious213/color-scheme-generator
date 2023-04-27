4622dc15f0559a0e1920a2e514ff962d9baf106e
vishalicious213 <vishalicious213@gmail.com>
2023-04-26 17:32:02 -0700

    added event listener for form submission

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const form = document.getElementById("form")
 const color = document.getElementById("color")
 const scheme = document.getElementById("scheme")
 const submitBtn = document.getElementById("submit")
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+// listen for form submissions
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    console.log("submitted")
+})
